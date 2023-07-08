017ec0bdb393264658ce690042e53c9e41ca1760
Cristian NITESCU <nitescuc@hotmail.com>
2023-07-08 15:17:25 +0200

    use single container config

---
+++ docker/openmower/scripts/start_mowgli_single.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Starting OpenMower"
+source /config/mower_config.sh
+/opt/ros/noetic/bin/roscore &
+/opt/ros/noetic/bin/rosrun rosserial_server serial_node _port:=/dev/mowgli _baud:=115200 &
+/opt/ros/noetic/bin/roslaunch open_mower open_mower.launch
