3b8e4c17f692289a0561037af61fe29b8686edd7
Cristian Nitescu <nitescuc@hotmail.com>
2023-10-18 20:46:27 +0200

    rosbridge launch fix

--- docker/openmower/scripts/start_mowgli_single.sh
+++ docker/openmower/scripts/start_mowgli_single.sh
@@ -4,4 +4,3 @@ source /config/mower_config.sh
 /opt/ros/noetic/bin/roscore &
 /opt/ros/noetic/bin/rosrun rosserial_server serial_node _port:=/dev/mowgli _baud:=115200 &
 /opt/ros/noetic/bin/roslaunch open_mower open_mower.launch --wait &
-/opt/ros/noetic/bin/roslaunch rosbridge_server rosbridge_websocket.launch --wait
